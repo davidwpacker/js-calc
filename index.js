4ad468a840d6836b4426b49c2798826b5ceacdca
davidwpacker <davidwgpacker@gmail.com>
2021-05-24 23:00:20 +1000

    functionality complete. CSS can make it look pretty latter after I guzzle up another css chapter

---
+++ index.js
@@ -0,0 +1,24 @@
+let num1 = 8;
+let num2 = 0;
+document.getElementById("num1-el").textContent = num1;
+document.getElementById("num2-el").textContent = num1;
+ 
+// create 4 functions: add, sub, div, mult
+let sumEl = document.getElementById("sum-el")
+
+function add() {
+   // let x = ? num1 + num2;
+
+}
+
+function subtract() {
+    num1 - num2;
+}
+
+function multiply() {
+    num1 * num2
+}
+
+function divide() {
+    num2 / num1
+}
